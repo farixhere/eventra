2a0621e2c59d1ab11f9cbb0a3f2ce3c3a78817c0
farixhere <dupekingsam@gmail.com>
2026-09-26 15:18:03 +0530

    Open Eventra dashboard for final project completion

--- app/dashboard/studio/page.js
+++ app/dashboard/studio/page.js
@@ -1,10 +1,3 @@
-import {cookies} from "next/headers";
-import {redirect} from "next/navigation";
 import EventStudioClient from "./EventStudioClient";
-import {COOKIE_NAME,parseUserToken} from "../../../lib/auth";
 export const dynamic="force-dynamic";
-export default async function EventStudioPage(){
- const user=await parseUserToken((await cookies()).get(COOKIE_NAME)?.value);
- if(!user) redirect("/dashboard/login?next=/dashboard/studio");
- return <EventStudioClient/>;
-}
+export default function EventStudioPage(){return <EventStudioClient/>;}
